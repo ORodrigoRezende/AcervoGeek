c39cb540d23858194b68ba05dc0483332b9a044f
Gabas2605 <gabarronamarcelly26@gmail.com>
2025-11-30 20:57:56 -0300

    Update JPrincipal.java
    Alterei!

--- visao/JPrincipal.java
+++ visao/JPrincipal.java
@@ -1,75 +1,181 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
-public class Janela extends JFrame {
+public class ProdutoForm extends JFrame implements ActionListener {
 
-  public Janela() {
-    super("Minha janela");
+  private JTextField tfnm, tfvl;
+  private JComboBox<String> cbct;
+  private JButton btsv, btal, btrm, btcn;
+  private DefaultTableModel modeloTabela;
+  private JTable tabela;
+
+  private String[] categorias = new String[] { "DIVERSOS", "LIMPEZA", "MANUTENÇÃO" };
+
+  public ProdutoForm() {
+
+    super("Formulário de produtos");
     setSize(800, 600);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setLayout(new BorderLayout());
+    setLayout(new GridBagLayout());
+    var gbc = new GridBagConstraints();
+    gbc.fill = GridBagConstraints.HORIZONTAL;
 
-    JButton bt1 = new JButton("Clique aqui");
-    add(bt1, BorderLayout.NORTH);
+    // linha 0
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.weightx = 0.0;
+    gbc.weighty = 0;
+    gbc.gridwidth = 1;
+    add(new JLabel("Nome:"), gbc);
 
-    JPanel painel = new JPanel(null);
-    add(painel, BorderLayout.CENTER);
+    tfnm = new JTextField();
+    gbc.gridx = 1;
+    gbc.gridy = 0;
+    gbc.weightx = 0.0;
+    gbc.weighty = 0;
+    gbc.gridwidth = 7;
+    add(tfnm, gbc);
 
-    JLabel lb1 = new JLabel("Nome:");
-    // lb1.setBounds(10, 10, 50, 20); //se o layout for null
-    JTextField tf1 = new JTextField("djshfs", 60);
-    // tf1.setBounds(70, 10, 100, 20); //se o layout for null
-    painel.add(lb1);
-    painel.add(tf1);
+    // linha 1
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    gbc.weightx = 0.0;
+    gbc.weighty = 0;
+    gbc.gridwidth = 1;
+    add(new JLabel("Valor:"), gbc);
 
-    JLabel lb2 = new JLabel("Descrição:");
-    JTextArea ta1 = new JTextArea(5, 30);
-    ta1.setFont(new Font("Arial", Font.BOLD, 20));
-    painel.add(lb2);
-    painel.add(ta1);
+    tfvl = new JTextField();
+    gbc.gridx = 1;
+    gbc.gridy = 1;
+    gbc.weightx = 0.0;
+    gbc.weighty = 0;
+    gbc.gridwidth = 3;
+    add(tfvl, gbc);
 
-    JCheckBox ck1 = new JCheckBox("Check?");
-    painel.add(ck1);
+    gbc.gridx = 4;
+    gbc.gridy = 1;
+    gbc.weightx = 0.0;
+    gbc.weighty = 0;
+    gbc.gridwidth = 1;
+    add(new JLabel("Categoria:"), gbc);
 
-    ButtonGroup bg1 = new ButtonGroup();
-    JRadioButton rb1 = new JRadioButton("Ligado", false);
-    JRadioButton rb2 = new JRadioButton("Desligado", true);
-    bg1.add(rb1);
-    bg1.add(rb2);
-    JPanel radioPanel = new JPanel(new FlowLayout());
-    radioPanel.setBorder(BorderFactory.createTitledBorder("Status"));
-    radioPanel.add(rb1);
-    radioPanel.add(rb2);
-    painel.add(radioPanel);
+    cbct = new JComboBox<String>(categorias);
+    cbct.setSelectedIndex(0);
+    gbc.gridx = 5;
+    gbc.gridy = 1;
+    gbc.weightx = 0.0;
+    gbc.weighty = 0;
+    gbc.gridwidth = 3;
+    add(cbct, gbc);
 
-    JComboBox<String> cb1 = new JComboBox<String>(new String[] { "OPCAO 1", "OPCAO 2" });
-    painel.add(cb1);
+    // linha 2
+    btsv = new JButton("Salvar");
+    btsv.addActionListener(this);
+    gbc.gridx = 0;
+    gbc.gridy = 2;
+    gbc.weightx = 0.5;
+    gbc.weighty = 0;
+    gbc.gridwidth = 2;
+    add(btsv, gbc);
 
-    JButton bt2 = new JButton("Clique aqui");
-    bt2.setPreferredSize(new Dimension(200, 100));
-    painel.add(bt2);
+    btal = new JButton("Alterar");
+    btal.addActionListener(this);
+    gbc.gridx = 2;
+    gbc.gridy = 2;
+    gbc.weightx = 0.5;
+    gbc.weighty = 0;
+    gbc.gridwidth = 2;
+    add(btal, gbc);
 
+    btrm = new JButton("Apagar");
+    btrm.addActionListener(this);
+    gbc.gridx = 4;
+    gbc.gridy = 2;
+    gbc.weightx = 0.5;
+    gbc.weighty = 0;
+    gbc.gridwidth = 2;
+    add(btrm, gbc);
+
+    btcn = new JButton("Cancelar");
+    btcn.addActionListener(this);
+    gbc.gridx = 6;
+    gbc.gridy = 2;
+    gbc.weightx = 0.5;
+    gbc.weighty = 0;
+    gbc.gridwidth = 2;
+    add(btcn, gbc);
+
+    // linha 3
+    modeloTabela = new DefaultTableModel();
+    modeloTabela.addColumn("Nome");
+    modeloTabela.addColumn("Valor");
+    modeloTabela.addColumn("Categoria");
+
+    tabela = new JTable(modeloTabela);
+    tabela.getColumnModel().getColumn(0).setMaxWidth(500);
+    tabela.getColumnModel().getColumn(1).setMaxWidth(100);
+    tabela.getColumnModel().getColumn(2).setMaxWidth(200);
+
+    var scroll = new JScrollPane();
+    scroll.setViewportView(tabela);
+    gbc.gridx = 0;
+    gbc.gridy = 3;
+    gbc.weightx = 0.0;
+    gbc.weighty = 1;
+    gbc.gridwidth = 8;
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.insets = new Insets(5, 0, 5, 0);
+    add(scroll, gbc);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+    if (e.getSource() == btsv) {
+      modeloTabela.addRow(new Object[] {
+          tfnm.getText(),
+          tfvl.getText(),
+          cbct.getSelectedItem()
+      });
+
+    } else if (e.getSource() == btal) {
+      for (int i = 0; i < tabela.getRowCount(); i++)
+        if (tabela.isRowSelected(i)) {
+          modeloTabela.setValueAt(tfnm.getText(), i, 0);
+          modeloTabela.setValueAt(tfvl.getText(), i, 1);
+          modeloTabela.setValueAt(cbct.getSelectedItem(), i, 2);
+          break;
+        }
+
+    } else if (e.getSource() == btrm) {
+      for (int i = 0; i < tabela.getRowCount(); i++)
+        if (tabela.isRowSelected(i))
+          modeloTabela.removeRow(i);
+
+    } else if (e.getSource() == btcn) {
+      tfnm.setText("");
+      tfvl.setText("");
+      cbct.setSelectedIndex(0);
+      tabela.clearSelection();
+    }
   }
 
   public static void main(String[] args) {
-    Janela j = new Janela();
-    j.setVisible(true);
+    new ProdutoForm().setVisible(true);
   }
 
 }
